ff6a7e8ac25e8577520f1f243a0e3be65bd81a29
Ezra <malsiezramicah3@gmail.com>
2024-07-20 23:02:48 +0800

    Slight modification

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    precacheImage(const AssetImage('lib/assets/icon/logo.png'), context);
+    precacheImage(const AssetImage('assets/icon/logo.png'), context);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
