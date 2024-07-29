f446a558254d2d33211e43bddd98a4cb7a53d0a9
Ezra <malsiezramicah3@gmail.com>
2024-07-29 12:05:09 +0800

    Removed Firebase to the app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
+
 import 'view/login_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
 
   runApp(const MyApp());
 }
