bb9b2ca9284bf64d304bcbed775112f4de6e0065
Ezra <malsiezramicah3@gmail.com>
2024-08-03 18:05:29 +0800

    Testing adding contents.

--- lib/view/page/home_page.dart
+++ lib/view/page/home_page.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import '../../widgets/custom_card.dart';
+import '../../widgets/leaderboard_card.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Home');
+    return const Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(0.0),
+          child: Column(
+            children: [
+              LeaderboardCard(),
+              CustomCard(
+                child: Column(
+                  children: [
+                    SizedBox(height: 20),
+                    // Add other widgets here if needed
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
