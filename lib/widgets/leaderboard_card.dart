c66f9233e800e97dd6b84ed8340c8fa1770698fd
Ezra <malsiezramicah3@gmail.com>
2024-08-04 00:21:15 +0800

    Separate the leaderboard position.

--- lib/widgets/leaderboard_card.dart
+++ lib/widgets/leaderboard_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'custom_card.dart';
+import 'leaderboard_position.dart';
 
 class LeaderboardCard extends StatelessWidget {
   const LeaderboardCard({super.key});
@@ -55,63 +56,6 @@ class LeaderboardCard extends StatelessWidget {
           const UserCard(),
         ],
       ),
-    );
-  }
-}
-
-class LeaderboardPosition extends StatelessWidget {
-  final int position;
-  final String name;
-  final String amount;
-  final Color color;
-  final bool isFirst;
-
-  const LeaderboardPosition({super.key,
-    required this.position,
-    required this.name,
-    required this.amount,
-    required this.color,
-    this.isFirst = false,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    double avatarRadius = isFirst ? MediaQuery.of(context).size.width * 0.12 : MediaQuery.of(context).size.width * 0.10;
-    double fontSize = isFirst
-        ? MediaQuery.of(context).size.width * 0.08
-        : MediaQuery.of(context).size.width * 0.06; // Adjust font size based on position
-
-    return Column(
-      children: [
-        Stack(
-          alignment: Alignment.topCenter,
-          children: [
-            CircleAvatar(
-              radius: avatarRadius,
-              backgroundColor: color,
-              child: Text(
-                '$position',
-                style: TextStyle(fontSize: fontSize, color: Colors.white),
-              ),
-            ),
-          ],
-        ),
-        const SizedBox(height: 10),
-        Text(
-          name,
-          style: TextStyle(
-            fontSize: MediaQuery.of(context).size.width * 0.04,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        Text(
-          amount,
-          style: TextStyle(
-            fontSize: MediaQuery.of(context).size.width * 0.04,
-            color: Colors.grey,
-          ),
-        ),
-      ],
     );
   }
 }
