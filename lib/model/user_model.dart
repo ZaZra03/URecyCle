3a95023ef8a8fa9a68a962293955a96334d6a78d
Ezra <malsiezramicah3@gmail.com>
2024-08-08 19:44:10 +0800

    Added role property.

--- lib/model/user_model.dart
+++ lib/model/user_model.dart
@@ -1,36 +1,25 @@
 class UserModel {
-  String email;
-  String password;
-  String firstName;
-  String lastName;
-  String studentNumber;
+  final String email;
+  final String firstName;
+  final String lastName;
+  final String studentNumber;
+  final String role;
 
   UserModel({
     required this.email,
-    required this.password,
     required this.firstName,
     required this.lastName,
     required this.studentNumber,
+    required this.role,
   });
 
-  // Convert a UserModel object into a map
-  Map<String, dynamic> toMap() {
-    return {
-      'email': email,
-      'firstName': firstName,
-      'lastName': lastName,
-      'studentNumber': studentNumber,
-    };
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      email: json['email'],
+      firstName: json['firstName'],
+      lastName: json['lastName'],
+      studentNumber: json['studentNumber'],
+      role: json['role'],
+    );
   }
-
-  // // Create a UserModel object from a map
-  // factory UserModel.fromMap(Map<String, dynamic> map) {
-  //   return UserModel(
-  //     email: map['email'],
-  //     password: '', // Do not store password in Firestore for security reasons
-  //     firstName: map['firstName'],
-  //     lastName: map['lastName'],
-  //     studentNumber: map['studentNumber'],
-  //   );
-  // }
 }
