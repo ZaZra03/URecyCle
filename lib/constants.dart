606a9b44070f6a42476943fcc6e949e2aa20e6e8
Ezra <malsiezramicah3@gmail.com>
2024-08-08 19:42:33 +0800

    Added new color and routes

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 class Constants {
   //Primary color
   static var primaryColor = const Color(0xFF5DB075);
-  static var secondaryColor = const Color(0xFF4B9460);
+  static var primary01Color = const Color(0xFF4B9460);
+  static var primary02Color = const Color(0xFF6ABF8B);
   static var blackColor = Colors.black54;
 
   //Gray color
@@ -17,6 +18,7 @@ class Constants {
   static const register = '${url}register';
   static const login = '${url}login';
   static const leaderboard = '${url}leaderboard';
+  static const user = '${url}user';
 
   //Onboarding texts
   static var titleOne = "Learn more about plants";
