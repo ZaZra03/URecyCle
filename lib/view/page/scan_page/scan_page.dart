ed2b085fb7dfc7c89bb46bb183de9bcf3ec4f3e7
Ezra Micah Malsi <malsiezramicah3@gmail.com>
2024-10-29 05:24:42 +0800

    Stable debug version of scan unit ver.2

--- lib/view/page/scan_page/scan_page.dart
+++ lib/view/page/scan_page/scan_page.dart
@@ -1,8 +1,22 @@
+// Flutter packages
 import 'dart:io';
-import 'dart:typed_data';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:pytorch_lite/pytorch_lite.dart';
+
+// Third-party packages
+import 'package:google_mlkit_image_labeling/google_mlkit_image_labeling.dart';
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
+import 'package:provider/provider.dart';
+
+// Local packages
+import 'package:urecycle_app/constants.dart';
+import 'package:urecycle_app/services/leaderboard_service.dart';
+import 'package:urecycle_app/services/transaction_service.dart';
+import 'package:urecycle_app/view/widget/loading_widget.dart';
+import '../../../provider/user_provider.dart';
+import '../../screen/user_screen.dart';
 
 class Scan extends StatefulWidget {
   const Scan({super.key});
@@ -12,96 +26,444 @@ class Scan extends StatefulWidget {
 }
 
 class _ScanState extends State<Scan> {
-  String? classificationResult;
-  Duration? classificationInferenceTime;
-  File? _image;
-  ClassificationModel? _imageModel;
-  bool _isLoading = false;
+  String _classificationResult = '';
+  bool _isProcessing = false;
+  ImageLabeler? _imageLabeler;
 
   @override
   void initState() {
     super.initState();
-    loadModel();
   }
 
-  Future<void> loadModel() async {
-    const pathImageModel = "assets/models/best_model.pt";
-    try {
-      _imageModel = await PytorchLite.loadClassificationModel(
-        pathImageModel, 224, 224, 6,
-        labelPath: "assets/labels/model.txt",
-      );
-    } catch (e) {
-      print("Error loading model: $e");
+  Future<String> getModelPath(String asset) async {
+    final path = '${(await getApplicationSupportDirectory()).path}/$asset';
+    await Directory(p.dirname(path)).create(recursive: true);
+    final file = File(path);
+    if (!await file.exists()) {
+      final byteData = await rootBundle.load(asset);
+      await file.writeAsBytes(byteData.buffer
+          .asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
     }
+    return file.path;
   }
 
-  Future<void> runModels(ImageSource source) async {
-    setState(() => _isLoading = true);
-
-    final ImagePicker picker = ImagePicker();
-    final XFile? pickedImage = await picker.pickImage(source: source);
-    if (pickedImage == null) {
-      setState(() => _isLoading = false);
-      return;
-    }
-
-    final File image = File(pickedImage.path);
-    final Uint8List imageBytes = await image.readAsBytes();
-
+  Future<void> _pickImageAndProcess() async {
     try {
-      final stopwatch = Stopwatch()..start();
-      List<double?>? predictionList = await _imageModel!.getImagePredictionList(
-        imageBytes,
-      );
-      classificationInferenceTime = stopwatch.elapsed;
-      print(predictionList);
-      int maxIndex = predictionList.indexWhere((e) => e == predictionList.reduce((a, b) => a! > b! ? a : b));
-      classificationResult = maxIndex >= 0 ? "Class $maxIndex" : "N/A";
+      final picker = ImagePicker();
+      final pickedFile = await picker.pickImage(source: ImageSource.gallery);
 
-      setState(() {
-        _image = image;
-        classificationResult = classificationResult;
+      if (pickedFile == null) {
+        print('No image selected.');
+        setState(() {
+          _isProcessing = false;
+          _classificationResult = 'No image selected.';
+        });
+        return;
+      }
+
+      // Set up model path and options
+      final modelPath = await getModelPath('assets/models/trashnet-quantized-metadata.tflite');
+      final options = LocalLabelerOptions(
+        confidenceThreshold: 0.5,
+        modelPath: modelPath,
+      );
+      _imageLabeler = ImageLabeler(options: options);
+
+      final inputImage = InputImage.fromFilePath(pickedFile.path);
+
+      await _imageLabeler!.processImage(inputImage).then((labels) async {
+        if (labels.isNotEmpty) {
+          final topLabel = labels.first;
+          final maxScore = topLabel.confidence;
+          final label = topLabel.label;
+
+          print("Object detected is $label");
+          print("Max Score is $maxScore");
+
+          if (maxScore <= 0.5) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const Unknown(result: 'Unknown')),
+            );
+          } else {
+            setState(() {
+              _classificationResult = label;
+            });
+
+            if (_classificationResult.isNotEmpty) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Recycle(result: _classificationResult)),
+              );
+            } else {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Trash(result: _classificationResult)),
+              );
+            }
+          }
+        }
+      }).catchError((e) {
+        print('Error processing image: $e');
+        setState(() {
+          _classificationResult = 'Error occurred while processing.';
+        });
       });
     } catch (e) {
-      print("Error during classification: $e");
+      print('Error: $e');
+      setState(() {
+        _classificationResult = 'Error occurred while processing.';
+      });
     } finally {
-      setState(() => _isLoading = false);
+      setState(() {
+        _isProcessing = false;
+      });
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Run Models')),
+      appBar: AppBar(title: const Text("Scan")),
       body: Center(
-        child: _isLoading
-            ? const CircularProgressIndicator()
-            : SingleChildScrollView(
+        child: ElevatedButton(
+          onPressed: _pickImageAndProcess,
+          child: const Text("Pick Image from Gallery"),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _isProcessing = false;
+    _imageLabeler?.close();
+    super.dispose();
+  }
+}
+
+
+class Trash extends StatelessWidget {
+  final String result;
+
+  const Trash({super.key, required this.result});
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Container(
+        color: Colors.grey[800],
+        child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              if (_image != null) ...[
-                const SizedBox(height: 20),
-                Image.file(_image!),
-                const SizedBox(height: 20),
-                Text(
-                  "Classification Result: ${classificationResult ?? "N/A"}",
-                  style: const TextStyle(fontSize: 16),
-                ),
-                Text(
-                  "Classification Time: ${classificationInferenceTime?.inMilliseconds ?? "N/A"} ms",
-                  style: const TextStyle(fontSize: 16),
-                ),
-                const SizedBox(height: 20),
-              ],
-              ElevatedButton(
-                onPressed: () => runModels(ImageSource.camera),
-                child: const Text('Take Photo & Run Models'),
+            children: <Widget>[
+              const Icon(
+                Icons.delete_outline,
+                size: 150,
+                color: Colors.white,
               ),
-              ElevatedButton(
-                onPressed: () => runModels(ImageSource.gallery),
-                child: const Text('Pick from Gallery & Run Models'),
+              const SizedBox(height: 20),
+              const Text(
+                'Waste Disposal Notice',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 10),
+              Text(
+                'Scanned Waste: $result',
+                style: const TextStyle(color: Colors.white, fontSize: 24),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                'Please dispose of it properly.',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 40),
+              Material(
+                elevation: 5,
+                borderRadius: BorderRadius.circular(30),
+                color: Colors.white,
+                child: InkWell(
+                  borderRadius: BorderRadius.circular(30),
+                  onTap: () {
+                    Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Scan()),
+                          (Route<dynamic> route) => false,
+                    );
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+                    width: screenWidth * 0.8,
+                    alignment: Alignment.center,
+                    child: const Text(
+                      "Go Back",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20), // Add space between buttons
+              Material(
+                elevation: 5,
+                borderRadius: BorderRadius.circular(30),
+                color: Colors.white,
+                child: InkWell(
+                  borderRadius: BorderRadius.circular(30),
+                  onTap: () {
+                    // Navigate to Learn More page or perform an action
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+                    width: screenWidth * 0.8,
+                    alignment: Alignment.center,
+                    child: const Text(
+                      "Learn More",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Unknown extends StatelessWidget {
+  final String result;
+
+  const Unknown({super.key, required this.result});
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Container(
+        color: Colors.grey[850],
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Icon(
+                Icons.help_outline,
+                size: 150,
+                color: Colors.white,
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Unknown Disposal Notice',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 10),
+              Text(
+                'Scanned Waste: $result',
+                style: const TextStyle(color: Colors.white, fontSize: 24),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                'This waste type is not recognized. Please check and dispose of it responsibly.',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 40),
+              Material(
+                elevation: 5,
+                borderRadius: BorderRadius.circular(30),
+                color: Colors.white,
+                child: InkWell(
+                  borderRadius: BorderRadius.circular(30),
+                  onTap: () {
+                    Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Scan()),
+                          (Route<dynamic> route) => false,
+                    );
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+                    width: screenWidth * 0.8,
+                    alignment: Alignment.center,
+                    child: const Text(
+                      "Go Back",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20), // Add space between buttons
+              Material(
+                elevation: 5,
+                borderRadius: BorderRadius.circular(30),
+                color: Colors.white,
+                child: InkWell(
+                  borderRadius: BorderRadius.circular(30),
+                  onTap: () {
+                    // Navigate to Learn More page or perform an action
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+                    width: screenWidth * 0.8,
+                    alignment: Alignment.center,
+                    child: const Text(
+                      "Learn More",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Recycle extends StatelessWidget {
+  final String result;
+
+  const Recycle({super.key, required this.result});
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Container(
+        color: Constants.primaryColor,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Icon(
+                Icons.workspace_premium_outlined,
+                size: 150,
+                color: Colors.white,
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Thanks for being eco-friendly!',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 10),
+              Text(
+                'Scanned Waste: $result(Recyclable)',
+                style: const TextStyle(color: Colors.white, fontSize: 24),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                'You have received 10 points!',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 40),
+              Material(
+                elevation: 5,
+                borderRadius: BorderRadius.circular(30),
+                color: Colors.white,
+                child: InkWell(
+                  borderRadius: BorderRadius.circular(30),
+                  onTap: () {
+                    Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Scan()),
+                          (Route<dynamic> route) => false,
+                    );
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+                    width: screenWidth * 0.8,
+                    alignment: Alignment.center,
+                    child: const Text(
+                      "Go Back",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20), // Add space between buttons
+              Material(
+                elevation: 5,
+                borderRadius: BorderRadius.circular(30),
+                color: Colors.white,
+                child: InkWell(
+                  borderRadius: BorderRadius.circular(30),
+                  onTap: () {
+                    // Navigate to Learn More page or perform an action
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+                    width: screenWidth * 0.8,
+                    alignment: Alignment.center,
+                    child: const Text(
+                      "Learn More",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
